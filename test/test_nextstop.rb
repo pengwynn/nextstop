d1312fafb86c8172a42047ac2f56db4882c3b7c4
Wynn Netherland <wynn@squeejee.com>
2009-11-18 07:22:37 -0600

    Added add'l tests

--- test/test_nextstop.rb
+++ test/test_nextstop.rb
@@ -1,7 +1,31 @@
 require 'helper'
 
-class TestNextstop < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+class TestReadernaut < Test::Unit::TestCase
+  context "When hitting the Nextstop API" do
+    setup do
+      @nextstop = Nextstop.new('OU812')
+    end
+    
+    should "return info for a place" do
+      stub_get "http://api.nextstop.com/p/nzp9KFPK4ok/?key=OU812", "place.json"
+      place = @nextstop.place('nzp9KFPK4ok')
+      place.address.should == '1801 N Griffin St'
+      place.keywords.size.should == 8
+    end
+    
+    should "return info for a guide" do
+      stub_get "http://api.nextstop.com/guide/nzp9KFPK4ok/?key=OU812", "guide.json"
+      guide = @nextstop.guide('nzp9KFPK4ok')
+      guide.permalink.should == 'http://www.nextstop.com/guide/nzp9KFPK4ok/alicias-favorites-in-the-dfw-metroplex/'
+      guide.guideContent['count'].should == 8
+    end
+    
+    should "search places" do
+      stub_get "http://api.nextstop.com/search/?q=dallas&result_type=places&key=OU812", "places_search.json"
+      results = @nextstop.search_places(:q => 'dallas')
+      results.totalResults.should == 100
+      results.resultsList.first.locationName.should == 'Dallas'
+    end
+    
   end
 end
